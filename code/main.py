d18871305ae8ba6cfc61aa49bc09f29221f4b119
Tiago D. Barbieri <tiagodbarbieri@hotmail.com>
2024-06-14 16:32:36 -0300

    Add INSS object

--- code/main.py
+++ code/main.py
@@ -1,20 +1,21 @@
 import math
+from tables import INSS
 
 
 def CALCULO_INSS(DADOS_INSS, SB):
-    INSS = 0.0
+    inss = 0.0
     for line in DADOS_INSS:
         if SB >= line[0] and SB <= line[1]:
-            INSS += (SB - line[0]) * (line[2] / 100)
-            return INSS
+            inss += (SB - line[0]) * (line[2] / 100)
+            return inss
         else:
-            INSS += (line[1] - line[0]) * (line[2] / 100)
-    return INSS
+            inss += (line[1] - line[0]) * (line[2] / 100)
+    return inss
 
 
-def CALCULO_IRPF(DADOS_IRPF, SB, INSS, DP, DEP, VPA):
+def CALCULO_IRPF(DADOS_IRPF, SB, inss, DP, DEP, VPA):
     # Cálculo Salário Base IRPF
-    SBA_IRPF = SB - INSS - (DP * DEP) - VPA
+    SBA_IRPF = SB - inss - (DP * DEP) - VPA
     for line in DADOS_IRPF:
         if SBA_IRPF >= line[0] and SBA_IRPF <= line[1]:
             IRPF = SBA_IRPF * (line[2] / 100) - line[3]
@@ -22,10 +23,8 @@ def CALCULO_IRPF(DADOS_IRPF, SB, INSS, DP, DEP, VPA):
 
 
 # fmt:off
-DADOS_INSS = [(0.0, 1320.0, 7.5),
-              (1319.99, 2571.29, 9.0),
-              (2571.28, 3856.94, 12.0),
-              (3856.93, 7507.49, 14.0)]
+INSS_table = INSS()
+DADOS_INSS = INSS_table.table
 
 DADOS_IRPF = [(0.0, 2112.0, 0.0, 0.0),
               (2112.01, 2826.65, 7.5, 158.4),
@@ -42,27 +41,27 @@ PA = float(input("Porcentagem pensão alimentícia [%]: "))
 OD = float(input("Outros descontos: R$"))
 
 # Calculo do o INSS
-INSS = CALCULO_INSS(DADOS_INSS, SB)
+inss = CALCULO_INSS(DADOS_INSS, SB)
 
 # Cálculo Pensão Alimentícia
 VPA = 0.0
 IRPF = 0.0
 for value in range(0, 10):
     # Calculo do IRPF
-    IRPF = CALCULO_IRPF(DADOS_IRPF, SB, INSS, DP, DEP, VPA)
+    IRPF = CALCULO_IRPF(DADOS_IRPF, SB, inss, DP, DEP, VPA)
     # Salário Líquido Parcial
-    SLP = SB - INSS - IRPF
+    SLP = SB - inss - IRPF
     # Valor pensão alimentícia
     VPA = SLP * (PA / 100)
 
 # Total de descontos
-TD = INSS + IRPF + VPA + OD
+TD = inss + IRPF + VPA + OD
 
 # Cálculo Salário Líquido
 SL = SB - TD
 
 print(f"\nSalário bruto: R${SB:.2f}")
-print(f"INSS: R${INSS:.2f}")
+print(f"INSS: R${inss:.2f}")
 print(f"IRPF: R${IRPF:.2f}")
 print(f"Pensão alimentícia: R${VPA:.2f}")
 print(f"Outros descontos: R${OD:.2f}")
