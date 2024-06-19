5d13efaff9b608152d6bc71cceee2e790a9fdc2b
Tiago D. Barbieri <tiagodbarbieri@hotmail.com>
2024-06-19 10:43:38 -0300

    Refactor IRPF --> irpf

--- code/main.py
+++ code/main.py
@@ -40,17 +40,17 @@ inss = CALCULO_INSS(DADOS_INSS, SB)
 
 # Cálculo Pensão Alimentícia
 VPA = 0.0
-IRPF = 0.0
+irpf = 0.0
 for value in range(0, 10):
     # Calculo do IRPF
     IRPF = CALCULO_IRPF(DADOS_IRPF, SB, inss, DP, DEP, VPA)
     # Salário Líquido Parcial
-    SLP = SB - inss - IRPF
+    SLP = SB - inss - irpf
     # Valor pensão alimentícia
     VPA = SLP * (PA / 100)
 
 # Total de descontos
-TD = inss + IRPF + VPA + OD
+TD = inss + irpf + VPA + OD
 
 # Cálculo Salário Líquido
 SL = SB - TD
