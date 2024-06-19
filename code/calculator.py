3d0cdd2710bc4fbd4405df2a6aa28fd956af6ccc
Tiago D. Barbieri <tiagodbarbieri@hotmail.com>
2024-06-19 13:50:36 -0300

    Refactor

--- code/calculator.py
+++ code/calculator.py
@@ -2,11 +2,11 @@ from tables import INSS, IRPF, DEP
 
 
 class Calculator:
-    def __init__(self, salary=0.0, dependents=0, pension=0.0, other_discounts=0.0):
+    def __init__(self, salary=0.0, dependents=0, pension_percentage=0.0, other_discounts=0.0):
         # Inputs
         self.salary = salary
         self.dependents = dependents
-        self.pension = pension
+        self.pension_percentage = pension_percentage
         self.other_discounts = other_discounts
 
         # Get INSS table from web
@@ -18,18 +18,23 @@ class Calculator:
         self.irpf_table = self.irpf_data.table
 
         # Get Dependent value from web
-        self.dep_data = DEP()
-        self.dep_value = self.dep_data.value
+        self.dependent_data = DEP()
+        self.dependent_value = self.dependent_data.value
 
         # Calculations
         self.calculate()
 
     def calculate(self):
+        # Initializing variables
         self.pension_value = 0.0
-        self.irpf = 0.0
-        self.inss = self.inss_calculation()
+        self.irpf_value = 0.0
+        self.inss_value = self.inss_calculation()
+
+        # Initializing loop calculation
         self.loop_calculation()
-        self.total_discounts = self.inss + self.irpf + self.pension_value + self.other_discounts
+
+        # Initializing others variables
+        self.total_discounts = self.inss_value + self.irpf_value + self.pension_value + self.other_discounts
         self.net_salary = self.salary - self.total_discounts
 
     def inss_calculation(self):
@@ -45,12 +50,12 @@ class Calculator:
     def loop_calculation(self):
         for v in range(0, 10):
             irpf = self.irpf_calculation()
-            self.irpf = irpf
-            self.net_salary = self.salary - self.inss - irpf
-            self.pension_value = self.net_salary * (self.pension / 100)
+            parcial_net_salary = self.salary - self.inss_value - irpf
+            self.pension_value = parcial_net_salary * (self.pension_percentage / 100)
+            self.irpf_value = irpf
 
     def irpf_calculation(self):
-        base_salary = self.salary - self.inss - (self.dependents * self.dep_value) - self.pension_value
+        base_salary = self.salary - self.inss_value - (self.dependents * self.dependent_value) - self.pension_value
         for line in self.irpf_table:
             if base_salary >= line[0] and base_salary <= line[1]:
                 irpf = base_salary * (line[2] / 100) - line[3]
@@ -62,8 +67,8 @@ if __name__ == "__main__":
 
     print("=" * 50)
     print(f"\nSalário bruto: R${c.salary:.2f}")
-    print(f"INSS: R${c.inss:.2f}")
-    print(f"IRPF: R${c.irpf:.2f}")
+    print(f"INSS: R${c.inss_value:.2f}")
+    print(f"IRPF: R${c.irpf_value:.2f}")
     print(f"Pensão alimentícia: R${c.pension_value:.2f}")
     print(f"Outros descontos: R${c.other_discounts:.2f}")
     print(f"Total de descontos: R${c.total_discounts:.2f}")
@@ -72,14 +77,14 @@ if __name__ == "__main__":
 
     c.salary = 3500
     c.dependents = 0
-    c.pension = 20
+    c.pension_percentage = 20
     c.other_discounts = 150
     c.calculate()
 
     print("=" * 50)
     print(f"\nSalário bruto: R${c.salary:.2f}")
-    print(f"INSS: R${c.inss:.2f}")
-    print(f"IRPF: R${c.irpf:.2f}")
+    print(f"INSS: R${c.inss_value:.2f}")
+    print(f"IRPF: R${c.irpf_value:.2f}")
     print(f"Pensão alimentícia: R${c.pension_value:.2f}")
     print(f"Outros descontos: R${c.other_discounts:.2f}")
     print(f"Total de descontos: R${c.total_discounts:.2f}")
