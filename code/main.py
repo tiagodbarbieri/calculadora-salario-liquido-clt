dca148c7ec4894f8ccd47bed4fc26a7ae9e06c05
Tiago D. Barbieri <tiagodbarbieri@hotmail.com>
2024-06-19 17:28:57 -0300

    Update main.py

--- code/main.py
+++ code/main.py
@@ -2,13 +2,15 @@ from engine import Calculator
 from tkinter import Tk
 
 
-class MainWindow(Tk):
+class Window(Tk):
     def __init__(self):
         super().__init__()
 
+        # Creating calculator
         self.engine = Calculator()
+
         self.mainloop()
 
 
 if __name__ == "__main__":
-    app = MainWindow()
+    app = Window()
