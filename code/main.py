040cd576be7a1ad176a5ca2a28030a53d724368b
Tiago D. Barbieri <tiagodbarbieri@hotmail.com>
2024-06-20 22:34:30 -0300

    Create MyEntry class

--- code/main.py
+++ code/main.py
@@ -3,6 +3,53 @@ from tkinter import Tk
 from tkinter import ttk
 
 
+class MyEntry(ttk.Frame):
+    def __init__(self, master, text, type: int | float):
+        super().__init__(master=master)
+        self.type = type
+        self.msg = ""
+        self.status = False
+        # Label - text
+        self.label_text = ttk.Label(master=self, text=text)
+        self.label_text.grid(column=0, row=0, sticky="e", padx=5, pady=5)
+        # Entry
+        self.entry = ttk.Entry(master=self, width=10, validate="focusout", validatecommand=self.function_select)
+        self.entry.grid(column=1, row=0)
+        # Label - message
+        self.label_msg = ttk.Label(master=self, text=self.msg, width=16, foreground="red")
+        self.label_msg.grid(column=3, row=0, sticky="w", padx=5, pady=5)
+
+    def check_int(self, value: str) -> bool:
+        if value.strip().isnumeric() or value == "":
+            self.status = True
+            self.msg = ""
+            self.label_msg.configure(text=self.msg)
+            return True
+        else:
+            self.status = False
+            self.msg = "<- deve ser inteiro!"
+            self.label_msg.configure(text=self.msg)
+            return False
+
+    def check_float(self, value) -> bool:
+        if value.strip().replace(",", "").replace(".", "").isnumeric() or value == "":
+            self.status = True
+            self.msg = ""
+            self.label_msg.configure(text=self.msg)
+            return True
+        else:
+            self.status = False
+            self.msg = "<- número inválido!"
+            self.label_msg.configure(text=self.msg)
+            return False
+
+    def function_select(self) -> bool:
+        if self.type == int:
+            return self.check_int(self.entry.get())
+        elif self.type == float:
+            return self.check_float(self.entry.get())
+
+
 class Window(Tk):
     def __init__(self):
         # Creating calculator
@@ -13,84 +60,45 @@ class Window(Tk):
         self.title("Calculadora salário líquido CLT")
 
         # Label and Entry 01 - salary [R$]
-        self.lbl_01 = ttk.Label(self, text="Salário bruto R$")
-        self.lbl_01.grid(column=0, row=0, sticky="e", padx=5, pady=5)
-        self.entry_01 = ttk.Entry(self, width=10)
-        self.entry_01.grid(column=1, row=0)
+        self.entry_02 = MyEntry(self, "Salário bruto R$", float)
+        self.entry_02.grid(column=0, row=0, sticky="e", columnspan=3)
 
         # Label and Entry 02 - dependents [qty]
-        self.lbl_02 = ttk.Label(self, text="Dependentes")
-        self.lbl_02.grid(column=0, row=1, sticky="e", padx=5, pady=5)
-        self.entry_02 = ttk.Entry(
-            self, width=10, validate="focusout", validatecommand=lambda: self.check_int(self.entry_02.get())
-        )
-        self.entry_02.grid(column=1, row=1)
+        self.entry_02 = MyEntry(self, "Dependentes", int)
+        self.entry_02.grid(column=0, row=1, sticky="e", columnspan=3)
 
         # Label and Entry 03 - pension percentage [%]
-        self.lbl_03 = ttk.Label(self, text="Pensão [%]")
-        self.lbl_03.grid(column=0, row=2, sticky="e", padx=5, pady=5)
-        self.entry_03 = ttk.Entry(self, width=10)
-        self.entry_03.grid(column=1, row=2)
+        self.entry_02 = MyEntry(self, "Pensão [%]", float)
+        self.entry_02.grid(column=0, row=2, sticky="e", columnspan=3)
 
         # Label and Entry 04 - other discounts [R$]
-        self.lbl_04 = ttk.Label(self, text="Outros descontos R$")
-        self.lbl_04.grid(column=0, row=3, sticky="e", padx=5, pady=5)
-        self.entry_04 = ttk.Entry(
-            self, width=10, validate="focusout", validatecommand=lambda: self.check_float(self.entry_04.get())
-        )
-        self.entry_04.grid(column=1, row=3)
+        self.entry_02 = MyEntry(self, "Outros descontos R$", float)
+        self.entry_02.grid(column=0, row=3, sticky="e", columnspan=3)
 
-        # Button 01 - Clear
+        # Button 01 - Save
+        self.btn_save = ttk.Button(self, text="Salvar", width=15, command=self.save)
+        self.btn_save.grid(column=0, row=4, padx=5, pady=5)
+
+        # Button 02 - Clear
         self.btn_clear = ttk.Button(self, text="Limpar", width=15, command=self.clear)
-        self.btn_clear.grid(column=3, row=0, padx=5, pady=5, rowspan=2)
+        self.btn_clear.grid(column=1, row=4, padx=5, pady=5)
 
-        # Button 02 - Calculate
+        # Button 03 - Calculate
         self.btn_calculate = ttk.Button(self, text="Calcular", width=15, command=self.calculate)
-        self.btn_calculate.grid(column=3, row=2, padx=5, pady=5, rowspan=2)
+        self.btn_calculate.grid(column=2, row=4, padx=5, pady=5)
 
         # Loop window
         self.mainloop()
 
-    def check_int(self, value: str) -> bool:
-        if value.strip().isnumeric():
-            print("SIM")
-            return True
-        else:
-            print("NÃO")
-            return False
-        # return True if value.strip().isnumeric() else False
-
-    def check_float(self, value: str) -> bool:
-        if value.strip().replace(",", "").replace(".", "").isnumeric():
-            print("SIM")
-            return True
-        else:
-            print("NÃO")
-            return False
+    def save(self):
+        pass
 
     def clear(self):
         pass
 
     def calculate(self):
-        # self.engine.salary = self.verify(self.entry_01.get(), float)
-        # self.engine.dependents = self.verify(self.entry_02.get(), int)
-        # self.engine.pension_percentage = self.verify(self.entry_03.get(), float)
-        # self.engine.other_discounts = self.verify(self.entry_04.get(), float)
         self.engine.calculate()
         self.report()
-
-    """
-    def verify(self, value: str, object_type: type):
-        if value == "":
-            return object_type(0)
-        else:
-            v = value.replace(",", "")
-            v = v.replace(".", "")
-            if v.isnumeric():
-                return object_type(value.replace(",", "."))
-            else:
-                return object_type(0)
-    """
 
     def report(self):
         print("=" * 50)
